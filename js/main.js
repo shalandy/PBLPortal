f1549acbd676982a87c7b5a942ae9adfbc65151a
shalandy <shalandy@berkeley.edu>
2018-02-02 21:24:59 -0800

    nuke everything but tabling

--- js/main.js
+++ js/main.js
@@ -12,29 +12,29 @@
 	// list of days, then time slots per day, then people per time slot
 	var json = [
 					[
-						["Yusuf Yamani", "Eliot Hsu"], 
+						["Yusuf Yamani", "Eliot Hsu"],
 						["Donna Ye", "David Pailet", "Zachary Osterland", "Sabrina Ni", "Random Person"],
-						["Ozair Khan", "Tonya Nguyen", "Rena Song", "Meejin Choi"], 
+						["Ozair Khan", "Tonya Nguyen", "Rena Song", "Meejin Choi"],
 						["Jessica Tzeng", "Leona Chen", "Tae Kim"]],
 					[
-						["Austin Kim", "Justin Kim", "Caroline Chen"], 
+						["Austin Kim", "Justin Kim", "Caroline Chen"],
 						["Linda Li", "Cynthia Luan", "Sean Park"],
-						["Hermes Ip", "Sarah Shin", "Paras Dave", "Annie Ouyang"], 
+						["Hermes Ip", "Sarah Shin", "Paras Dave", "Annie Ouyang"],
 						["Vanessa Lin", "Wenbo Guo", "Arash Nassirinia"]],
 					[
-						["Taylor Wang", "Bob Feng", "Corey Gibbel", "Hyeonjo Jeong"], 
+						["Taylor Wang", "Bob Feng", "Corey Gibbel", "Hyeonjo Jeong"],
 						["Emily Zhu", "Jack Jones", "Linda Zheng"],
-						["Nicholas Paul", "Samantha Fang", "Younghoon Oh"], 
+						["Nicholas Paul", "Samantha Fang", "Younghoon Oh"],
 						["Emeri Zhang", "Tia Marie Vo"]],
 					[
-						["Lindsay Yang", "Stephanie Chen", "Emily Jin", "Eric Sung"], 
+						["Lindsay Yang", "Stephanie Chen", "Emily Jin", "Eric Sung"],
 						["Coco Jiang", "Sonia Park"],
-						["Shalandy Zhang", "Megan Wang", "Yichu Chen"], 
+						["Shalandy Zhang", "Megan Wang", "Yichu Chen"],
 						["Alex Park", "Anjanaa Senthil Kumar", "Alice Cho"]],
 					[
-						["Copter Ruangrojanin", "Cyssi Ngo", "Kristy Kao", "Carrington Park"], 
+						["Copter Ruangrojanin", "Cyssi Ngo", "Kristy Kao", "Carrington Park"],
 						["Sunbin Kim", "Nancy Chen", "Chloe Lin", "Howard Yin"],
-						["Arnold Chan", "Mikuto Murayama", "Roy Kamineni", "Josia Yuan"], 
+						["Arnold Chan", "Mikuto Murayama", "Roy Kamineni", "Josia Yuan"],
 						["Brian Zeng", "Amy Lu", "Nathan Orwig"]]];
 
 
@@ -53,6 +53,7 @@
 	});
 
 	var hrefs = [$('.js-button').attr('href'), 'https://goo.gl/forms/elEmOKavGFXGk2QF3'];
+	//var hrefs = [$('.js-button2').attr('href'), 'https://goo.gl/forms/elEmOKavGFXGk2QF3'];
 	$(window).on('resize', function() {
 		$('.js-button').attr('href', function() {
 			return hrefs[$(window).width() > 737 ? 0 : 1];
@@ -69,7 +70,7 @@
 			var table1 = $(this).parent().parent().parent();
 			var table2 = $(this).parent().parent();
 			var verTable = $(table1).data('vertable')+"";
-			var column = $(this).data('column') + ""; 
+			var column = $(this).data('column') + "";
 
 			$(table2).find("."+column).addClass('hov-column-'+ verTable);
 			$(table1).find(".row100.head ."+column).addClass('hov-column-head-'+ verTable);
@@ -82,12 +83,12 @@
 			var table1 = $(this).parent().parent().parent();
 			var table2 = $(this).parent().parent();
 			var verTable = $(table1).data('vertable')+"";
-			var column = $(this).data('column') + ""; 
+			var column = $(this).data('column') + "";
 
 			$(table2).find("."+column).removeClass('hov-column-'+ verTable);
 			$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
 		}
 	});
-    
+
 
 })(jQuery);
